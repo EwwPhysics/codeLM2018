b04c38d1f113600f8bf8bd076c87e8f0c9e37940
EwwPhysics <sashakmurray@gmail.com>
2021-02-05 18:28:39 -0500

    Rename for clarity
    Questions are now in order.

--- 02_waveCoin.py
+++ 02_waveCoin.py
@@ -1,4 +1,3 @@
 def wavecoin(num):
     a = [int(x) for x in str(num)]
     return sum(a) == 56
-
--- 06_new_products.py
+++ 06_new_products.py
@@ -4,5 +4,5 @@ from collections import Counter
 def s(products):
     counts = Counter(products).most_common()
     mx = counts[0][1]
-    test = filter(lambda x: x[1] == mx, counts)
-    return [item[0] for item in test]
+    max_products = [product[0] for product in filter(lambda x: x[1] == mx, counts)]
+    return max_products
