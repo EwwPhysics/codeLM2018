5c2165171b3c47ea510642afa62bcd6612ea4609
EwwPhysics <sashakmurray@gmail.com>
2021-02-05 12:00:15 -0500

    Update problem 6 to use collections.Counter

--- 6_new_products.py
+++ 6_new_products.py
@@ -1,14 +1,8 @@
-def solve(products):
-    product = {}
-    for i in products:
-        a = product.get(i, 0) + 1
-        product[i] = a
-    count = 0
-    result = []
-    for key, value in product.items():
-        if value > count:
-            result = [key]
-            count = value
-        elif value == count:
-            result.append(key)
-    return result
+from collections import Counter
+
+
+def s(products):
+    counts = Counter(products).most_common()
+    mx = counts[0][1]
+    test = filter(lambda x: x[1] == mx, counts)
+    return [item[0] for item in test]
