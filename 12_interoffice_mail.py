7e02b13b587af16556d5357b408e890938a5b1d0
EwwPhysics <sashakmurray@gmail.com>
2021-02-07 11:54:34 -0500

    Improve solution
    Now uses string instead of arbitrary one-item list.
    I'm not even completely sure if it works. I guess that's
    what happens when you only receive 3 basic test cases and
    are too lazy to create your own.

--- 12_interoffice_mail.py
+++ 12_interoffice_mail.py
@@ -1,22 +1,17 @@
-def solve(connections, path, sol="", sols=[]):
-    start = path[0]
-    end = path[1]
-    sol += start
+def solve(connections, path, s="", sol=''):
+    start, end = path
+    s += start
+
     if path in connections:
-        s = sol + end
-        try:
-            if len(s) < len(min(sols, key=lambda x: len(x))):
-                return [s]
-            else:
-                return sols
-        except ValueError:
-            return [s]
+        ans = s + end
+        if not sol:
+            return ans
+        elif len(ans) < len(sol):
+            return ans
+        return sol
+
     else:
-        c = filter(lambda x: x[0] == start, connections)
-        c = list(c)
+        c = list(filter(lambda x: x[0] == start, connections))
         for p in c:
-            sols = solve(connections, p[1] + end, sol, sols=sols)
-        return sols[0]
-
-
-print(solve(['AB', 'BC', 'CD', 'BD'], "AD"))
+            sol = solve(connections, p[1] + end, s, sol)
+        return sol
